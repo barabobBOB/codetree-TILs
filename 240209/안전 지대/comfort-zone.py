a2e84c8431666147c25e44772aae2feabf114053
최세연 (Lena) <choi20014830@gmail.com>
2024-02-09 23:38:53 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240209/안전 지대/comfort-zone.py
+++ 240209/안전 지대/comfort-zone.py
@@ -20,7 +20,7 @@ def dfs(x, y, k):
 
     return True
 
-K = 0
+K = 1
 max_safe = 0
 
 for k in range(1, 101):
@@ -31,7 +31,7 @@ for k in range(1, 101):
             if not visited[j][i] and graph[j][i] > k:
                 if dfs(i, j, k):
                     cnt += 1
-    if max_safe <= cnt:
+    if max_safe < cnt:
         K = k
         max_safe = cnt
 
