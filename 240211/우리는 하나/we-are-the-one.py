c20d1aca982584d8840b1ae97cc9fb811d892ef2
최세연 (Lena) <choi20014830@gmail.com>
2024-02-11 07:52:50 +0900

    Title: 우리는 하나, Time: 248ms, Memory: 27MB, Status: Passed - CodeTree

--- 240211/우리는 하나/we-are-the-one.py
+++ 240211/우리는 하나/we-are-the-one.py
@@ -7,8 +7,8 @@ graph = []
 for _ in range(N):
     graph.append(list(map(int, input().split())))
 
-def bfs(start):
-    visited = [[False] * N for _ in range(N)]
+def bfs(start, visited):
+    # visited = [[False] * N for _ in range(N)]
     count = 1  # 시작 도시 포함
 
     q = deque([start])
@@ -19,14 +19,18 @@ def bfs(start):
 
     while q:
         x, y = q.popleft()
+
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
+
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 h_diff = abs(graph[x][y] - graph[nx][ny])
+
                 if U <= h_diff <= D:
                     visited[nx][ny] = True
                     q.append((nx, ny))
                     count += 1
+
     return count
 
 max_count = 0
@@ -34,8 +38,9 @@ all_positions = [(i, j) for i in range(N) for j in range(N)]
 
 for combo in combinations(all_positions, K):
     total = 0
+    visited = [[False] * N for _ in range(N)]
     for pos in combo:
-        total += bfs(pos)
+        total += bfs(pos, visited)
     max_count = max(max_count, total)
 
 print(max_count)
