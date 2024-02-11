0dc7d708cd2b758685bfe67d637387579fb33952
최세연 (Lena) <choi20014830@gmail.com>
2024-02-12 07:19:24 +0900

    Title: 최소 경로로 탈출 하기, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240212/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240212/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -3,19 +3,14 @@ from collections import deque
 N, M = map(int, input().split())
 
 graph = []
-
-for _ in range(M):
+for _ in range(N):  # N행의 입력을 받음
     graph.append(list(map(int, input().split())))
 
-visited = [[False] * N for _ in range(M)]
+visited = [[False] * M for _ in range(N)]  # N행 M열로 visited 초기화
 
 def bfs(x, y):
-    if graph[-1][-1] == 0:
-        return -1
-
     q = deque()
     q.append((x, y))
-
     visited[y][x] = True
     
     dx = [1, 0, -1, 0]
@@ -28,16 +23,13 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx <= N - 1 and 0 <= ny <= M - 1:
+            if 0 <= nx < M and 0 <= ny < N:
                 if not visited[ny][nx] and graph[ny][nx] == 1:
                     visited[ny][nx] = True
                     graph[ny][nx] = graph[y][x] + 1
                     q.append((nx, ny))
 
-    if graph[-1][-1] == 1:
-            return -1
-
-    return graph[-1][-1] - 1
+    return graph[-1][-1] if graph[-1][-1] != 1 else -1 
 
 result = bfs(0, 0)
 print(result)
