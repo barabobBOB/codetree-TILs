e612cb8f471334b48b1d76e5c99269efae910da2
최세연 (Lena) <choi20014830@gmail.com>
2024-02-12 21:58:30 +0900

    Title: 뿌요뿌요, Time: 140ms, Memory: 26MB, Status: Passed - CodeTree

--- 240212/뿌요뿌요/puyo-puyo.py
+++ 240212/뿌요뿌요/puyo-puyo.py
@@ -11,19 +11,17 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 max_count = 0
-count = 1
 
 def dfs(x, y, k):
     visited[y][x] = True
-    global count
+    count = 1  # Start counting from the current node
 
     for i in range(4):
         nx = dx[i] + x
         ny = dy[i] + y
 
         if 0 <= nx < N and 0 <= ny < N and not visited[ny][nx] and graph[ny][nx] == k:
-            count += 1
-            dfs(nx, ny, k)
+            count += dfs(nx, ny, k)  # Accumulate the count from recursive calls
 
     return count
 
@@ -32,10 +30,9 @@ c = 0
 for i in range(N):
     for j in range(N):
         if not visited[i][j]:
-            cnt = dfs(i, j, graph[i][j])
-            count = 0
-            max_count = max(max_count, cnt)
+            cnt = dfs(j, i, graph[i][j])  # Call DFS for each unvisited node
             if cnt >= 4:
                 c += 1
+            max_count = max(max_count, cnt)  # Update the max count if needed
 
 print(c, max_count)
